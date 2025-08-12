dd858dc2215fb00f481f56fc10bfccf75d56bac5
wangjiabao <840028401@qq.com>
2025-08-12 09:02:43 +0800

    ok

--- internal/data/user.go
+++ internal/data/user.go
@@ -500,6 +500,7 @@ func (u *UserRepo) CreateCardTwo(ctx context.Context, userId uint64, user *biz.U
 	cardTwo.Email = user.Email
 	cardTwo.CountryCode = user.CountryCode
 	cardTwo.Country = user.Country
+	cardTwo.City = user.City
 
 	resInsertTwo := u.data.DB(ctx).Table("card_two").Create(&reward)
 	if resInsertTwo.Error != nil || 0 >= resInsertTwo.RowsAffected {
